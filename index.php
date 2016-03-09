808e2d2f17b5cc29392578cfb2f2e90feffae495
Kristina Zvjezina <kristina.zvjezina@skole.hr>
2016-03-09 13:36:12 +0100

    Dodali smo PHP kod i varijable.

--- index.php
+++ index.php
@@ -2,14 +2,24 @@
 <html>
 <head>
     <title>Prvi PHP</title>
+    <meta charset="utf-8"
 </head>
 
 <body>
 <h1>Prvi PHP</h1>
 
 <?php
-echo('Ovo je prije odlomka<br>');
-    echo('Super, jos cemo dodati');
+    echo('Ovo je prije odlomka<br>');
+    echo('<br>');
+    echo('Super, još ćemo dodati<br>');
+
+    echo(date('d.m.Y.'));
+
+    $ocjena = 5;
+    echo('<br>Vaša ocjena je : ' . $ocjena);
+
+    $ocjena = 1;
+    echo('<br> Vaša ocjena je: ' . $ocjena)
 ?>
 
 <p>
