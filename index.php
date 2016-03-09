312028628f2a36ac76e6da7b3122bf5392db88a4
Kristina Zvjezina <kristina.zvjezina@skole.hr>
2016-03-09 13:18:28 +0100

    Prvi commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prvi PHP</title>
+</head>
+
+<body>
+<h1>Prvi PHP</h1>
+
+<?php
+echo('Ovo je prije odlomka<br>');
+    echo('Super, jos cemo dodati');
+?>
+
+<p>
+    Ovo je odlomak
+</p>
+</body>
+</html>
